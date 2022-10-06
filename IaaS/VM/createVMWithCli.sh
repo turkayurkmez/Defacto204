55fc649ec6f15263945cd05ccae27172265e58f3
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-10-06 13:54:37 +0300

    first script

--- IaaS/VM/createVMWithCli.sh
+++ IaaS/VM/createVMWithCli.sh
@@ -2,3 +2,6 @@
 az login
 # ardından çalışacağınız subscription'u belirtin (Özellikle Defacto hesabınızla girdiyseniz :))
 az account set --subscription "Azure Sponsorluk"
+
+#1. rg'i listeleyin
+az group list --output table
