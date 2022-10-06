fc3bb300199d08afc925c57b728b6bacb6550c3a
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-10-06 12:30:06 +0300

    vm cli script started

---
+++ VM/createVMWithCli.sh
@@ -0,0 +1,4 @@
+# önce azure shell üzerine login olun
+az login
+# ardından çalışacağınız subscription'u belirtin (Özellikle Defacto hesabınızla girdiyseniz :))
+az account set --subscription "Azure Sponsorluk"
