6af60f023555f918bd59fdea069661420dcb20ac
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-10-06 14:03:45 +0300

    creating vm via cli

--- IaaS/VM/createVMWithCli.sh
+++ IaaS/VM/createVMWithCli.sh
@@ -5,3 +5,13 @@ az account set --subscription "Azure Sponsorluk"
 
 #1. rg'i listeleyin
 az group list --output table
+#1.1 gerekirse rg oluşturun:
+az group create --name bademo-vm-rg-2 --location "westeurope"
+# 2. VM oluştur
+az vm create \
+     --resource-group "bademo-vm-rg-2" \
+     --name "bademo-win-cli" \
+     --image "win2019datacenter" \
+     --admin-username "turkay" \
+     --admin-password "Pa55W0rd1234"
+
